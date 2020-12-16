9a7754b1036bcb55da5a2aa2736538f6d03f18af
joki <jonas.kiefer@live.com>
2020-12-16 17:00:57 +0100

    Get sonarcloud running issue #5 * updates sonar-project.properties file * moves test reports to tests/coverage-reports

--- tests/app/settings.py
+++ tests/app/settings.py
@@ -17,9 +17,9 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 NOSE_ARGS = [
     '--with-xunit',
-    '--xunit-file=tests/nosetests.xml',
+    '--xunit-file=tests/coverage-reports/xunit-report.xml',
     '--with-coverage',
     '--cover-erase',
     '--cover-xml',
-    '--cover-xml-file=tests/nosecover.xml',
+    '--cover-xml-file=tests/coverage-reports/coverage-report.xml',
 ]
